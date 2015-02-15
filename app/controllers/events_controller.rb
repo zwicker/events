e3ff56eadc467d4bb6d39d9bdd93276a50407f19
zwicker <ememery@localhost.localdomain>
2015-02-15 13:20:28 +1100

    Added Data to Controller Array

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,5 @@
 class EventsController < ApplicationController
   def index
-    @events = ['Bugsmash', 'Hackathon', 'Kata Camp']
+    @events = ['Bugsmash', 'Hackathon', 'Kata Camp', 'Rails User Group']
   end
 end
